46480d2e04ca0834276529788c7a98d914f3f873
Vadim Volodin <vad.e.volodin@gmail.com>
2021-07-28 17:31:01 +0300

    tmp

--- tests/queries/0_stateless/02000_table_function_cluster_macros.sql
+++ tests/queries/0_stateless/02000_table_function_cluster_macros.sql
@@ -1,2 +1,3 @@
-CREATE TABLE test_db_table_function_cluster_macros ON CLUSTER '{cluster}' (date DateTime, value UInt32) engine=MergeTree() ORDER BY date
-SELECT * FROM cluster("{cluster}", default.test_db_table_function_cluster_macros);
+CREATE TABLE test_db_table_function_cluster_macros ON CLUSTER "{default_cluster}" (date DateTime, value UInt32) engine=MergeTree() ORDER BY date
+SELECT * FROM cluster("{default_cluster}", default.test_db_table_function_cluster_macros)
+SELECT * FROM clusterAllReplicas("{default_cluster}", default.test_db_table_function_cluster_macros)
