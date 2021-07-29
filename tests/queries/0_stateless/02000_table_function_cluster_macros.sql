59a28d1abbf7125f1107e65dc9899008c063ea16
Vadim Volodin <vad.e.volodin@gmail.com>
2021-07-29 03:50:57 +0300

    update

--- tests/queries/0_stateless/02000_table_function_cluster_macros.sql
+++ tests/queries/0_stateless/02000_table_function_cluster_macros.sql
@@ -1,3 +1,2 @@
-CREATE TABLE test_db_table_function_cluster_macros ON CLUSTER "{default_cluster}" (date DateTime, value UInt32) engine=MergeTree() ORDER BY date
-SELECT * FROM cluster("{default_cluster}", default.test_db_table_function_cluster_macros)
-SELECT * FROM clusterAllReplicas("{default_cluster}", default.test_db_table_function_cluster_macros)
+SELECT _shard_num FROM cluster("{default_cluster_macro}", system.one);
+SELECT _shard_num FROM clusterAllReplicas("{default_cluster_macro}", system.one);
