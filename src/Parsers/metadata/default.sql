8013d989b27d0a33df1058e905dcb860477095a0
Vadim Volodin <vadim-volodin@qavm-ff2ff183.qemu>
2022-04-16 02:03:29 +0300

    remove rubbish

--- src/Parsers/metadata/default.sql
+++
@@ -1,2 +0,0 @@
-ATTACH DATABASE _ UUID 'd6dcc8ab-cc74-4c8e-aee2-b1fa55cfe9d7'
-ENGINE = Atomic
--- src/Storages/MergeTree/store/9b2/9b238f06-ca0a-4d9f-a3e8-e54e5111800c/X.sql
+++
@@ -1,7 +0,0 @@
-ATTACH TABLE _ UUID 'e1cb0213-bcca-409f-b3dc-9aa10d803a43'
-(
-    `hehe` String
-)
-ENGINE = MergeTree
-ORDER BY hehe
-SETTINGS index_granularity = 8192
